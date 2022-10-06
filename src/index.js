83e2b9867b8884fe525fbf7ea77a59fd901427f9
eusouregislima <danilolima.cogic@gmail.com>
2022-10-05 23:45:56 -0300

    estilos globais

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import Login from './Container/Login'
+import GlobalStyles from './styles/globalstyles'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<Login />)
+root.render(
+  <>
+    <Login />
+    <GlobalStyles />
+  </>
+)
---
+++ src/styles/globalstyles.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Roboto', sans-serif;
+  text-decoration: none;
+  border: none;
+  outline: none;
+  transition: 0.3s ease;
+}
+`
